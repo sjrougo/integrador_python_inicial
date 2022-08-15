44fb8fb7fedc48e3878799f38adc3004ea10646b
sjrougo <107430928+sjrougo@users.noreply.github.com>
2022-08-15 17:32:41 -0300

    Add files via upload

--- integrador_python_inicial.py
+++ integrador_python_inicial.py
@@ -1,8 +1,21 @@
-from ast import Try
 import csv
-from enum import auto
-from operator import getitem
-from tkinter import N
+
+def abrir_archivo_lectura (modo): #Esta función realiza la apertura del archivo colocando los datos en una variable list
+    '''El parametro modo indica el modo en el que se abrirá el archivo (w, r, a, r+)'''
+    data = {}
+
+    with open('Archivo_biblioteca.csv', modo, encoding='utf-8') as csvfile:
+        data = list(csv.DictReader(csvfile))
+
+    return data
+
+def escribir_archivo (modo, data):
+    if modo == 'w':
+        with open('Archivo_biblioteca.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            header = ['id', 'ISBN', 'Nombre', 'Autor', 'Género']
+            writer = csv.DictWriter(csvfile, fieldnames = header)
+            writer.writeheader()
+            writer.writerows(data)
 
 def generar_id():
     '''
@@ -12,42 +25,24 @@ def generar_id():
     # Aumentar en 1 el ID encontrado, y retornarlo
     return ultimo_id + 1
     '''
-    with open('Archivo_biblioteca.csv', 'r', encoding='utf-8') as csvfile:
-        # Leer archivo CSV y almacenar los resultados en data
-        data = list(csv.DictReader(csvfile))
 
     # Obtener ultima fila del CSV leído
-    ultimo_id = len(data)
+    ultimo_id = len(abrir_archivo_lectura('r'))
 
     # Aumentar en 1 el ID encontrado, y retornarlo
     return ultimo_id + 1
-
 
 def reordenar_ids():
     '''
     Función que reordena los IDs del CSV de libros
     No tiene valor de retorno. 
     '''
-
-    with open('Archivo_biblioteca.csv', 'r', encoding='utf-8') as csvfile:
-        data = list(csv.DictReader(csvfile))
-
-    for i in range(len(data)):
+    data = abrir_archivo_lectura('r')
+    for i in range(len(abrir_archivo_lectura('r'))):
         # Asignar como id de un libro, su índice en la lista + 1, para no asignarle a nadie el ID = 0
         data[i]['id'] = i + 1
 
-    with open('Archivo_biblioteca.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        # Especificar cuales serán las columnas del CSV
-        header = ['id', 'ISBN', 'Nombre', 'Autor', 'Género']
-
-        # Construir el objeto writer, que se encargará de escribir el csv
-        writer = csv.DictWriter(csvfile, fieldnames = header)
-
-        # Escribir los nombres de las columnas
-        writer.writeheader()
-
-        # Escribir los libros 
-        writer.writerows(data)
+    escribir_archivo('w', data)
 
 
 def nuevo_libro():
@@ -55,9 +50,9 @@ def nuevo_libro():
     isbn_existe = False
     variable_sn = None
 
+    
     while control:
-        with open('Archivo_biblioteca.csv', 'r', encoding='utf-8') as csvfile:
-            data = list(csv.DictReader(csvfile))
+        data = abrir_archivo_lectura('r')
         
         csvfile = open('Archivo_biblioteca.csv', 'a', newline='', encoding='utf-8')
         header = ['id', 'ISBN', 'Nombre', 'Autor', 'Género'] # Se detallan los nombres del encabezado
@@ -112,52 +107,52 @@ def nuevo_libro():
                         print("Debe Ingresar un valor válido")
         csvfile.close()
 
-
 def borrar_libro():
     id = None
     diccionario = {}
 
-    with open('Archivo_biblioteca.csv', 'r', encoding='utf-8') as csvfile:
-        data = list(csv.DictReader(csvfile))
+    data = abrir_archivo_lectura('r')
+    dict = data[0]
 
-    print ("Estos son los libros que tiene en su biblioteca:")
+    print("Estos son los libros que tiene en su biblioteca:")
+    print('Id    Nombre              Autor')
     for diccionario in data:
         print(diccionario.get('id'), '  ', diccionario.get('Nombre'), '  ', diccionario.get ('Autor'))
-    id = int(input("Elija el Id del libro que desea borrar.\n")) - 1
-    data.pop(id)
-    
-    with open('Archivo_biblioteca.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        header = ['id', 'ISBN', 'Nombre', 'Autor', 'Género'] # Se detallan los nombres del encabezado
-        writer = csv.DictWriter(csvfile, fieldnames=header)
-        writer.writeheader()
-        writer.writerows(data)
+    while True:
+        print("Elija el Id del libro que desea borrar:")
+        id = int(input()) - 1
+        if id > 0 and id < len(data):
+            data.pop(id)
+            break
+        else:
+            print("Debe ingresar un valor válido!")
+    escribir_archivo('w', data)
     reordenar_ids()
-
 
 def modificar_libro():
     id = None
 
-    with open('Archivo_biblioteca.csv', 'r', encoding='utf-8') as csvfile:
-        data = list(csv.DictReader(csvfile))
+    data = abrir_archivo_lectura('r')
     
     print ("Estos son los libros que tiene en su biblioteca:")
+    print('Id   ISBN         Nombre            Autor           Género')
     for diccionario in data:
         print(diccionario.get('id'), '  ',diccionario.get('ISBN'), '  ', diccionario.get('Nombre'), '  ', diccionario.get ('Autor'), '  ', diccionario.get('Género'))
-    id = int(input("Elija el Id del libro que desea modificar.\n"))
+    while True:
+        print("Elija el Id del libro que desea borrar:")
+        id = int(input()) - 1
+        if id > 0 and id < len(data):
+            data.pop(id)
+            break
+        else:
+            print("Debe ingresar un valor válido!")
     isbn = str(input("Ingrese el ISBN del libro:\n")) # Se solicita al usuario ingrese la información
     nombre_libro = str(input("Ingrese nombre del libro:\n")) # de un libro
     autor_libro = str(input("Ingrese autor del libro:\n"))
     genero_libro = str(input("Ingrese género del libro:\n"))
-    dict_dato = {'id': id, 'ISBN': isbn, 'Nombre': nombre_libro, 'Autor': autor_libro, 'Género': genero_libro}
-    data.pop(id - 1)
-    data.insert(id - 1, dict_dato)
-
-    with open('Archivo_biblioteca.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        header = ['id', 'ISBN', 'Nombre', 'Autor', 'Género'] # Se detallan los nombres del encabezado
-        writer = csv.DictWriter(csvfile, fieldnames=header)
-        writer.writeheader()
-        writer.writerows(data)
-
+    dict_dato = {'id': id + 1, 'ISBN': isbn, 'Nombre': nombre_libro, 'Autor': autor_libro, 'Género': genero_libro}
+    data.insert(id, dict_dato)
+    escribir_archivo('w', data)
 
 def reportes_varios():
     opcion_reporte = None
@@ -166,26 +161,29 @@ def reportes_varios():
     genero = None
     isbn = None
 
-    with open('Archivo_biblioteca.csv', 'r', encoding='utf-8') as csvfile:
-        data = list(csv.DictReader(csvfile))
+    data = abrir_archivo_lectura('r')
 
     while True:
         opcion_reporte = input("Ingrese una opción:\n1.- Listado general.\n2.- Listado por autor.\n3.- Listado por género.\n4.- Libro por ISBN.\n")
         if opcion_reporte == '1':
+            print('Id   ISBN        Nombre          Autor          Género')
             for diccionario in data:
                 print(diccionario.get('id'), '  ',diccionario.get('ISBN'), '  ', diccionario.get('Nombre'), '  ', diccionario.get ('Autor'), '  ', diccionario.get('Género'))
         elif opcion_reporte == '2':
             autor = str(input('Ingrese nombre del autor:\n'))
+            print('Id   ISBN        Nombre          Autor           Género')
             for diccionario in data:
                 if diccionario.get('Autor') == autor:
                     print(diccionario.get('id'), '  ',diccionario.get('ISBN'), '  ', diccionario.get('Nombre'), '  ', diccionario.get ('Autor'), '  ', diccionario.get('Género'))
         elif opcion_reporte == '3':
             genero = str(input('Ingrese género:\n'))
+            print('Id   ISBN          Nombre           Autor          Género')
             for diccionario in data:
                 if diccionario.get('Género') == genero:
                     print(diccionario.get('id'), '  ',diccionario.get('ISBN'), '  ', diccionario.get('Nombre'), '  ', diccionario.get ('Autor'), '  ', diccionario.get('Género'))
         elif opcion_reporte == '4':
             isbn = str(input('Ingrese ISBN a buscar:\n'))
+            print('Id   ISBN       Nombre          Autor        Género')
             for diccionario in data:
                 if diccionario.get('ISBN') == isbn:
                     print(diccionario.get('id'), '  ',diccionario.get('ISBN'), '  ', diccionario.get('Nombre'), '  ', diccionario.get ('Autor'), '  ', diccionario.get('Género'))
